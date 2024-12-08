4646c0c2968400b566beeca969ed3cea4241eeb3
RealWooblay <jackaustinc66@gmail.com>
2024-12-08 17:21:52 +1100

    revert

--- frontend/src/app/components/hedera/walletConnect.js
+++ frontend/src/app/components/hedera/walletConnect.js
@@ -11,25 +11,15 @@ async function walletConnectFcn() {
 		pairedWalletData: null,
 		pairedAccounts: [],
 	};
-
 	let appMetadata = {
 		name: "Hashcalls",
 		description: "HTS Options Marketplace",
 		icon: "https://raw.githubusercontent.com/ed-marquez/hedera-dapp-days/testing/src/assets/hederaLogo.png",
 	};
-
 	let hashconnect = new HashConnect();
 
-	// Attempt to load previously saved data (if any) from localStorage
-	const savedData = localStorage.getItem("hashconnectData") || "";
-	const savedJson = savedData ? JSON.parse(savedData) : undefined;
-
-	// Pass the restored data to init
-	const initData = await hashconnect.init(appMetadata, savedJson, "testnet");
-
-	// After initializing, update localStorage with the latest state
-	localStorage.setItem("hashconnectData", JSON.stringify(hashconnect.hcData));
-
+	// First init and store the pairing private key for later (this is NOT your account private key)
+	const initData = await hashconnect.init(appMetadata);
 	saveData.privateKey = initData.privKey;
 	console.log(`- Private key for pairing: ${saveData.privateKey}`);
 
@@ -41,7 +31,7 @@ async function walletConnectFcn() {
 	// Generate a pairing string, which you can display and generate a QR code from
 	saveData.pairingString = hashconnect.generatePairingString(state, "testnet", false);
 
-	// Find any supported local wallets and attempt to connect
+	// Find any supported local wallets
 	hashconnect.findLocalWallets();
 	hashconnect.connectToLocalWallet(saveData.pairingString);
 
